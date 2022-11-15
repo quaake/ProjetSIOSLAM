31e1aa90168c1712d0c4d24badc7b664227ab407
Nass269 <97893123+Nass269@users.noreply.github.com>
2022-11-15 16:48:03 +0100

    Add files via upload

--- Model/fonction.php
+++ Model/fonction.php
@@ -23,16 +23,16 @@
 	}
 
 	function SelectUnCritere($table, $champ1, $critere1)
-{
-    $connect=Connexion();
+	{
+		$connect=Connexion();
 
-    $result=$connect->prepare("SELECT * FROM ".$table." WHERE ".$champ1." =:critere1");
+		$result=$connect->prepare("SELECT * FROM ".$table." WHERE ".$champ1." =:critere1");
 
-    $result->execute(array(':critere1'=>$critere1));
+		$result->execute(array(':critere1'=>$critere1));
 
-    Deconnexion($connect);
-    return $result;
-}
+		Deconnexion($connect);
+		return $result;
+	}
 
 	function SelectClub($table, $id, $loguser)
 	{
@@ -44,5 +44,22 @@
 
     	Deconnexion($connect);
     	return $result;
+	}
+
+	function InsertBindParam($table, $Champ1, $Champ2, $Champ3, $Champ4, $Champ5, $Champ6,$Champ7, $Champ8,$Champ9) {
+		$connect=Connexion();
+		$result = $connect->prepare('INSERT INTO '.$table .' VALUE (:Champ1, :Champ2, :Champ3, :Champ4, :Champ5, :Champ6, :Champ7, :Champ8, :Champ9)');
+		$result -> bindParam(':Champ1', $Champ1, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ2', $Champ2, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ3', $Champ3, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ4', $Champ4, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ5', $Champ5, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ6', $Champ6) ;
+		$result -> bindParam(':Champ7', $Champ7, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ8', $Champ8, PDO ::PARAM_STR) ;
+		$result -> bindParam(':Champ9', $Champ9, PDO ::PARAM_STR) ;
+		$result -> execute();
+		Deconnexion($connect);
+		return $result;
 	}
 ?>
