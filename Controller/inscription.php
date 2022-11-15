46b43cae95e224f9e40984e66eb7ff96d7797338
Nass269 <97893123+Nass269@users.noreply.github.com>
2022-11-15 16:20:19 +0100

    Add files via upload

--- Controller/inscription.php
+++ Controller/inscription.php
@@ -2,10 +2,22 @@
 
 	include("../Model/fonction.php");
     $title = "Inscription";
+    $table = "adherent";
+    $str = "Location:connexion.php";
 
-    $sql = "INSERT INTO MyGuests (Mdp, Nom, Prenom, DateNaissance,Email,AdresseRue,AdresseCP,AdresseVille)
-    VALUES ('John', 'Doe', 'john@example.com')";
+    if(isset($_POST['btnValid']) && $_POST['btnValid'] =='Valider' && $_POST['mdp'] == $_POST['mdpConf']){
+        $mail = filter_input(INPUT_POST, 'Email', FILTER_VALIDATE_EMAIL);
+        $hashMDP = password_hash($_POST['mdp'], PASSWORD_BCRYPT);
+        $newAdherent = InsertAdherent($mail, $hashMDP, $_POST['Nom'], $_POST['prenom'], $_POST['Date'], $_POST['adresse'], $_POST['cp'], $_POST['ville']);
+        if($newAdherent){
+            header($str);
+        }
+        else{
+            echo "Erreur lors de la création du nouvel adhérent !";
+        }
 
+        
+}
 
  
 
--- View/inscription.view.php
+++ View/inscription.view.php
@@ -6,33 +6,33 @@
                 <form action="" method="POST" id="formcon">
                 
                     <label for="name">Email: </label>
-                    <input type="email" placeholder="Email" id="Email" required/>
+                    <input type="email" placeholder="Email" id="Email" name="Email" required/>
                 
                     <label for="email">Mot de passe: </label>
-                    <input type="password" placeholder="Mot de passe" id="Mot-de-passe" required/>
+                    <input type="password" placeholder="Mot de passe" id="Mot-de-passe" name="mdp" required/>
 
                     <label for="email">Confirmer: </label>
-                    <input type="password" placeholder="Confirmation Du Mot de passe" name="Mot-de-passe" id="Mot-de-passe" required/>
+                    <input type="password" placeholder="Confirmation Du Mot de passe" name="mdpConf" id="Mot-de-passe" required/>
 
                     <label for="email">Nom: </label>
-                    <input type="text" placeholder="Nom" id="Nom" required/>
+                    <input type="text" placeholder="Nom" id="Nom" name="Nom" required/>
 
                     <label for="email">Prenom: </label>
-                    <input type="text" placeholder="prenom" id="prenom" required/>
+                    <input type="text" placeholder="prenom" id="prenom" name="prenom" required/>
 
                     <label for="email">Date de Naissance: </label>
-                    <input type="date" placeholder="Date" id="Date" required/>
+                    <input type="date" placeholder="Date" id="Date" name="Date" required/>
 
                     <label for="email">Adresse:</label>
-                    <input type="text" placeholder="adresse" id="adresse" required/>
+                    <input type="text" placeholder="adresse" id="adresse" name="adresse" required/>
                     
                     <label for="email">Code Postal: </label>
-                    <input type="text" placeholder="cp" id="cp" required/>
+                    <input type="text" placeholder="cp" id="cp" name="cp" required/>
 
                     <label for="email">Ville: </label>
-                    <input type="text" placeholder="ville" id="ville" required/>
+                    <input type="text" placeholder="ville" id="ville" name="ville" required/>
 
-                    <button type="submit" value="">S'inscrire!</button>
+                    <button type="submit" value="Valider" name="btnValid">S'inscrire!</button>
                 
             </form>
 <?php
